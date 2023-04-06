cb9cafa1e88c27e8617cec547de38b513b78c4ac
sifat-e-noor <sifatenu@gmail.com>
2023-04-06 18:20:51 +0200

    Fix the welcome page

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';  
+import { StatusBar } from 'expo-status-bar'; //This is a component that we can use to show the status bar at the top of the screen
+import { StyleSheet, Text, View, Image, SafeAreaView, ImageBackground } from 'react-native';  //Importing the components we need
+
+const image = require("./assets/base.png"); //Importing the image from the assets folder
 
 //In react native we dont use raw html -> view is like <div
 //SafeAreaView adds padding and makes sure that our content isn't cut.
 //This is platform independent (f.ex View -> UIView in iOS when we run it).
 export default function App() {
   const handlePress = () => console.log("Text pressed")
-  console.log(require("./assets/logo.png"));
+  console.log(require("./assets/app_welcome_text.png"));
 
   return (
-    <SafeAreaView style={styles.container}> 
-      <Text onPress={handlePress}>Welcome to T-Minus!</Text> 
-      <Image source={require("./assets/logo.png")} />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <ImageBackground source={image} style={styles.image}>
+      <SafeAreaView style={styles.container}> 
+        {/* <Text onPress={handlePress}>Welcome to T-Minus!</Text>  */} 
+          <Image onPress={handlePress} source={require("./assets/app_welcome_text.png")} />
+          <StatusBar style="auto" />
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
@@ -21,8 +25,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: { // This is just a regular JavaScript property not CSS
     flex: 1, //This makes the view flow to the entire screen
-    backgroundColor: '#217BF9',
+    // backgroundColor: '#217BF9', // We are using image instead of background color
     alignItems: 'center', //Next two lines make sure that 
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
     justifyContent: 'center',
   },
 });
